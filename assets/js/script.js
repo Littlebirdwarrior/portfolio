15743ce93dc189fe79aee1c6a875c84dfed1d9f3
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2022-06-07 11:33:46 +0200

    commit-section hero well advanced

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+//Stars
+
+function createSquare(){
+    const section = document.querySelector('.stars_section');
+    const square = document.createElement('b');
+    let size = Math.random() * -10;
+
+    square.style.width = 8 + size + 'px';
+    square.style.height = 8 + size + 'px';
+
+    square.style.top = Math.random() * innerHeight +'px';
+    square.style.left = Math.random() * innerWidth +'px';
+
+    section.appendChild(square);
+
+}
+setInterval(createSquare , 2000)
+
+
+
